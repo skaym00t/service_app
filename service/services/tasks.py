e338dae87239a4d6381cdbca3b3c9cb70888ba38
Skaym00T <cerber_lv@mail.ru>
2025-05-02 16:56:40 +0300

    Fix bugs

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -5,9 +5,11 @@ from celery import shared_task
 @shared_task # Декоратор для создания задачи Celery (можно использовать любое имя)
 def set_price(subscription_id): # Функция задачи(можно использовать любое имя)
     from services.models import Subscription
+
     subscription = Subscription.objects.get(id=subscription_id) # Получаем подписку по id
     new_price = (subscription.service.full_price -
                  subscription.service.full_price * subscription.plan.discount_percent / 100) # Получаем новую цену подписки,
     # c вычетом скидки
     subscription.price = new_price # Присваиваем новую цену подписки
     subscription.save(save_model=False) # Сохраняем подписку(не вызывая метод save у модели Subscription)
+    return "Success"
